9e3cca326d7a4e8ff5cd7db33c0c33911beb64f6
Waylon Brown <waylonjbrown@gmail.com>
2014-03-24 00:08:34 -0500

    player can now move around

--- RIP.js
+++ RIP.js
@@ -13,10 +13,36 @@ function init() {
  * are only ever created once
  */
 var imageRepository = new function() {
-	// Define images
+
 	this.background = new Image();
+	this.player = new Image();
+	//this.bullet = new Image();
+
+	// Ensure all images have loaded before starting the game
+	var numImages = 3;
+	var numLoaded = 0;
+
+	function imageLoaded() {
+		numLoaded++;
+		if (numLoaded === numImages) {
+			window.init();
+		}
+	}
+
+	this.background.onload = function() {
+		imageLoaded();
+	}
+	this.player.onload = function() {
+		imageLoaded();
+	}
+	//this.bullet.onload = function() {
+	//	imageLoaded();
+	//}
+
 	// Set images src
 	this.background.src = "images/grass-tiled.png";
+	this.player.src = "images/player.png";
+	//this.background.src = "images/bullet.png";
 }
 
 /**
@@ -26,10 +52,12 @@ var imageRepository = new function() {
  * functions. 
  */
 function Drawable() {	
-	this.init = function(x, y) {
+	this.init = function(x, y, width, height) {
 		// Defualt variables
 		this.x = x;
 		this.y = y;
+		this.width = width;
+		this.height = height;
 	}
 
 	this.speed = 0;
@@ -38,6 +66,8 @@ function Drawable() {
 
 	// Define abstract function to be implemented in child objects
 	this.draw = function() {
+	};
+	this.move = function() {
 	};
 }
 
@@ -56,6 +86,69 @@ function Background() {
 // Set Background to inherit properties from Drawable
 Background.prototype = new Drawable();
 
+function Player() {
+	this.speed = 3;
+	//this.bulletPool = new Pool(30);
+	//this.bulletPool.init();
+
+	//var fireRate = 15;
+	var counter = 0;
+
+	this.draw = function() {
+		this.context.drawImage(imageRepository.player, this.x, this.y);
+	};
+	this.move = function() {	
+		counter++;
+		// Determine if the action is move action
+		if (KEY_STATUS.left || KEY_STATUS.right ||
+			KEY_STATUS.down || KEY_STATUS.up) {
+			// The player moved, so erase it's current image so it can
+			// be redrawn in it's new location
+			this.context.clearRect(this.x, this.y, this.width, this.height);
+
+			// Update x and y according to the direction to move and
+			// redraw the player. Change the else if's to if statements
+			// to have diagonal movement.
+			if (KEY_STATUS.left) {
+				this.x -= this.speed
+				if (this.x <= 0) // Keep player within the screen
+					this.x = 0;
+			} else if (KEY_STATUS.right) {
+				this.x += this.speed
+				if (this.x >= this.canvasWidth - this.width)
+					this.x = this.canvasWidth - this.width;
+			} else if (KEY_STATUS.up) {
+				this.y -= this.speed
+				// if (this.y <= this.canvasHeight/4*3)
+				// 	this.y = this.canvasHeight/4*3;
+				if (this.y <= 0)
+					this.y = 0;
+			} else if (KEY_STATUS.down) {
+				this.y += this.speed
+				if (this.y >= this.canvasHeight - this.height)
+					this.y = this.canvasHeight - this.height;
+			}
+
+			// Finish by redrawing the player
+			this.draw();
+		}
+
+		//if (KEY_STATUS.space && counter >= fireRate) {
+		//	this.fire();
+		//	counter = 0;
+		//}
+	};
+
+	/*
+	 * Fires two bullets
+	 */
+	//this.fire = function() {
+	//	this.bulletPool.getTwo(this.x+6, this.y, 3,
+	//	                       this.x+33, this.y, 3);
+	//};
+}
+Player.prototype = new Drawable();
+
 /**
  * Creates the Game object which will hold all objects and data for
  * the game.
@@ -69,12 +162,17 @@ function Game() {
 	 * running on older browsers.
 	 */
 	this.init = function() {
-		// Get the canvas element
+		// Get the canvas elements
 		this.bgCanvas = document.getElementById('background');
+		this.playerCanvas = document.getElementById('players');
+		this.mainCanvas = document.getElementById('bullets');
 
-		// Test to see if canvas is supported
+		// Test to see if canvas is supported. Only need to
+		// check one canvas
 		if (this.bgCanvas.getContext) {
 			this.bgContext = this.bgCanvas.getContext('2d');
+			this.playerContext = this.playerCanvas.getContext('2d');
+			//this.mainContext = this.mainCanvas.getContext('2d');
 
 			// Initialize objects to contain their context and canvas
 			// information
@@ -82,9 +180,26 @@ function Game() {
 			Background.prototype.canvasWidth = this.bgCanvas.width;
 			Background.prototype.canvasHeight = this.bgCanvas.height;
 
+			Player.prototype.context = this.playerContext;
+			Player.prototype.canvasWidth = this.playerCanvas.width;
+			Player.prototype.canvasHeight = this.playerCanvas.height;
+
+			//Bullet.prototype.context = this.mainContext;
+			//Bullet.prototype.canvasWidth = this.mainCanvas.width;
+			//Bullet.prototype.canvasHeight = this.mainCanvas.height;
+
 			// Initialize the background object
 			this.background = new Background();
 			this.background.init(0,0); // Set draw point to 0,0
+
+			// Initialize the player object
+			this.player = new Player();
+			// Set the player to start near the bottom middle of the canvas
+			var playerStartX = this.playerCanvas.width/2 - imageRepository.player.width;
+			var playerStartY = this.playerCanvas.height/2;// + imageRepository.player.height*2;
+			this.player.init(playerStartX, playerStartY, imageRepository.player.width,
+			               imageRepository.player.height);
+
 			return true;
 		} else {
 			return false;
@@ -93,9 +208,11 @@ function Game() {
 
 	// Start the animation loop
 	this.start = function() {
+		this.player.draw();
 		animate();
 	};
 }
+
 
 
 /**
@@ -107,8 +224,54 @@ function Game() {
 function animate() {
 	requestAnimFrame( animate );
 	game.background.draw();
+	game.player.move();
+	//game.player.bulletPool.animate(); 
 }
 
+KEY_CODES = {
+  32: 'space',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+}
+
+// Creates the array to hold the KEY_CODES and sets all their values
+// to false. Checking true/flase is the quickest way to check status
+// of a key press and which one was pressed when determining
+// when to move and which direction.
+KEY_STATUS = {};
+for (code in KEY_CODES) {
+  KEY_STATUS[KEY_CODES[code]] = false;
+}
+/**
+ * Sets up the document to listen to onkeydown events (fired when
+ * any key on the keyboard is pressed down). When a key is pressed,
+ * it sets the appropriate direction to true to let us know which
+ * key it was.
+ */
+document.onkeydown = function(e) {
+  // Firefox and opera use charCode instead of keyCode to
+  // return which key was pressed.
+  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+  if (KEY_CODES[keyCode]) {
+	e.preventDefault();
+	KEY_STATUS[KEY_CODES[keyCode]] = true;
+  }
+}
+/**
+ * Sets up the document to listen to ownkeyup events (fired when
+ * any key on the keyboard is released). When a key is released,
+ * it sets teh appropriate direction to false to let us know which
+ * key it was.
+ */
+document.onkeyup = function(e) {
+  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+  if (KEY_CODES[keyCode]) {
+    e.preventDefault();
+    KEY_STATUS[KEY_CODES[keyCode]] = false;
+  }
+}
 
 /**	
  * requestAnim shim layer by Paul Irish
