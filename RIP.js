5f3c6f00be6a219f75b77d96dadf1319ce91ad9f
Garrett <insanit.ignis@gmail.com>
2014-03-26 17:21:01 -0500

    Edited Directories & uploaded files to server
    Server URL "students.cse.tamu.edu/ggrams14"

--- RIP.js
+++ RIP.js
@@ -40,9 +40,9 @@ var imageRepository = new function() {
 	//}
 
 	// Set images src
-	this.background.src = "images/grass-tiled.png";
-	this.player.src = "images/player.png";
-	//this.background.src = "images/bullet.png";
+	this.background.src = "images/backgrounds/grass-tiled.png";
+	this.player.src = "images/players/player.png";
+	//this.background.src = "images/objects/bullet.png";
 }
 
 /**
