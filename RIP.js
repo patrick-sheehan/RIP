49454ceb1f0087b110ce8517ed2b85b5d7c34e19
Waylon Brown <waylonjbrown@gmail.com>
2014-03-31 00:22:05 -0500

    bullets shoot with incorrect angle

--- RIP.js
+++ RIP.js
@@ -1,290 +1,201 @@
-/**
- * Initialize the Game and starts it.
- */
-var game = new Game();
+var stage;
+var player;
+var background;
+var canvas;
 
-function init() {
-	if(game.init())
-		game.start();
+var upKey = 87;
+var downKey = 83;
+var leftKey = 65; 
+var rightKey = 68;
+
+var upPressed;
+var downPressed;
+var leftPressed;
+var rightPressed;
+var mousePressed = false;
+
+var movementSpeed = 5;
+var bulletFireRate = 20;	//higher the rate, slower bullet shoots
+var bulletFrameCounter = 0;
+var bulletSpeed = 15;
+
+var bulletArray = [];
+
+document.onkeydown = handleKeyDown;
+document.onkeyup = handleKeyUp;
+
+function init()
+{
+	canvas = document.getElementById("gameCanvas");
+	stage = new createjs.Stage(canvas);
+
+	background = new createjs.Bitmap("images/backgrounds/grass-tiled.png");
+	stage.addChild(background);
+
+	createjs.Ticker.setFPS(60);
+	createjs.Ticker.addEventListener("tick", tick);
+	stage.addEventListener("stagemousedown", mouseClick);
+	stage.addEventListener("stagemouseup", mouseUnclick);
+	player = new Player();
+
+	stage.update();
 }
 
-/**
- * Define an object to hold all our images for the game so images
- * are only ever created once
- */
-var imageRepository = new function() {
+function tick(event)
+{
+	movePlayer();
+	rotatePlayer();
+	if(mousePressed)
+		playerShoot();
+	moveBullets();
+	stage.update();
+}
 
-	this.background = new Image();
-	this.player = new Image();
-	//this.bullet = new Image();
+function Player()
+{
+	this.image = new createjs.Bitmap("images/player.png");
+	this.image.x = Math.random()*canvas.width;
+	this.image.y = Math.random()*canvas.height;
+	//set registration points to center of image
+	this.image.regX = 50;
+	this.image.regY = 50;
+	stage.addChild(this.image);
+}
 
-	// Ensure all images have loaded before starting the game
-	var numImages = 3;
-	var numLoaded = 0;
+function Bullet()
+{
+	this.image = new createjs.Bitmap("images/bullet.png");
+	this.image.x = player.image.x;
+	this.image.y = player.image.y;
+	this.initialX = this.image.x;
+	this.initialY = this.image.y;
+	this.angle = player.image.rotation;
+	console.log("bullet count: " + bulletArray.length);
+	stage.addChild(this.image);
+}
 
-	function imageLoaded() {
-		numLoaded++;
-		if (numLoaded === numImages) {
-			window.init();
-		}
+function movePlayer()
+{
+	//doing each combination, as if you detect them separately then the player moves sqrt(2) as fast on diagonals
+	if(upPressed && leftPressed)
+	{
+		player.image.y -= movementSpeed / 1.4;
+		player.image.x -= movementSpeed / 1.4;
+	}
+	else if(upPressed && rightPressed)
+	{
+		player.image.y -= movementSpeed / 1.4;
+		player.image.x += movementSpeed / 1.4;
+	}
+	else if(rightPressed && downPressed)
+	{
+		player.image.x += movementSpeed / 1.4;
+		player.image.y += movementSpeed / 1.4;
+	}
+	else if(leftPressed && downPressed)
+	{
+		player.image.x -= movementSpeed / 1.4;
+		player.image.y += movementSpeed / 1.4;
+	}
+	else if(upPressed)
+	{
+		player.image.y -= movementSpeed;
+	}
+	else if(downPressed)
+	{
+		player.image.y += movementSpeed;
+	}
+	else if(leftPressed)
+	{
+		player.image.x -= movementSpeed;
+	}
+	else if(rightPressed)
+	{
+		player.image.x += movementSpeed;
 	}
 
-	this.background.onload = function() {
-		imageLoaded();
+	if(player.image.x < 0)
+	{
+		player.image.x = 0;
 	}
-	this.player.onload = function() {
-		imageLoaded();
+	if(player.image.y < 0)
+	{
+		player.image.y = 0;
 	}
-	//this.bullet.onload = function() {
-	//	imageLoaded();
-	//}
-
-	// Set images src
-	this.background.src = "images/backgrounds/grass-tiled.png";
-	this.player.src = "images/players/player_up.png";
-	//this.background.src = "images/objects/bullet.png";
+	if(player.image.x  > canvas.width)
+	{
+		player.image.x = canvas.width;
+	}
+	if(player.image.y > canvas.height)
+	{
+		player.image.y = canvas.height;
+	}
 }
 
-/**
- * Creates the Drawable object which will be the base class for
- * all drawable objects in the game. Sets up default variables
- * that all child objects will inherit, as well as the defualt
- * functions. 
- */
-function Drawable() {	
-	this.init = function(x, y, width, height) {
-		// Default variables
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
+function moveBullets()
+{
+	for(var i = 0; i < bulletArray.length; i++)
+	{
+		bulletArray[i].image.x = Math.cos(bulletArray[i].angle) * bulletSpeed  + bulletArray[i].initialX;
+		bulletArray[i].image.y = Math.sin(bulletArray[i].angle) * bulletSpeed + bulletArray[i].initialY;
+		bulletArray[i].initialX = bulletArray[i].image.x;
+		bulletArray[i].initialY = bulletArray[i].image.y;
 	}
 
-	this.speed = 0;
-	this.canvasWidth = 0;
-	this.canvasHeight = 0;
-
-	// Define abstract function to be implemented in child objects
-	this.draw = function() {
-	};
-	this.move = function() {
-	};
 }
 
-/**
- * Creates the Background object which will become a child of
- * the Drawable object. The background is drawn on the "background"
- * canvas \
- */
-function Background() {
-
-	// Implement abstract function
-	this.draw = function() {
-		this.context.drawImage(imageRepository.background, this.x, this.y);
-	};
-}
-// Set Background to inherit properties from Drawable
-Background.prototype = new Drawable();
-
-function Player() {
-	this.speed = 3;
-	//this.bulletPool = new Pool(30);
-	//this.bulletPool.init();
-
-	//var fireRate = 15;
-	var counter = 0;
-
-	this.draw = function() {
-		this.context.drawImage(imageRepository.player, this.x, this.y);
-	};
-	this.move = function() {	
-		counter++;
-		// Determine if the action is move action
-		if (KEY_STATUS.left || KEY_STATUS.right ||
-			KEY_STATUS.down || KEY_STATUS.up) {
-			// The player moved, so erase it's current image so it can
-			// be redrawn in it's new location
-			this.context.clearRect(this.x, this.y, this.width, this.height);
-
-			// Update x and y according to the direction to move and
-			// redraw the player. Change the else if's to if statements
-			// to have diagonal movement.
-			if (KEY_STATUS.left) {
-				this.x -= this.speed
-				if (this.x <= 0) // Keep player within the screen
-					this.x = 0;
-			} else if (KEY_STATUS.right) {
-				this.x += this.speed
-				if (this.x >= this.canvasWidth - this.width)
-					this.x = this.canvasWidth - this.width;
-			} else if (KEY_STATUS.up) {
-				this.y -= this.speed
-				// if (this.y <= this.canvasHeight/4*3)
-				// 	this.y = this.canvasHeight/4*3;
-				if (this.y <= 0)
-					this.y = 0;
-			} else if (KEY_STATUS.down) {
-				this.y += this.speed
-				if (this.y >= this.canvasHeight - this.height)
-					this.y = this.canvasHeight - this.height;
-			}
-
-			// Finish by redrawing the player
-			this.draw();
-		}
-
-		//if (KEY_STATUS.space && counter >= fireRate) {
-		//	this.fire();
-		//	counter = 0;
-		//}
-	};
-
-	/*
-	 * Fires two bullets
-	 */
-	//this.fire = function() {
-	//	this.bulletPool.getTwo(this.x+6, this.y, 3,
-	//	                       this.x+33, this.y, 3);
-	//};
-}
-Player.prototype = new Drawable();
-
-/**
- * Creates the Game object which will hold all objects and data for
- * the game.
- */
-function Game() {
-	/*
-	 * Gets canvas information and context and sets up all game
-	 * objects. 
-	 * Returns true if the canvas is supported and false if it
-	 * is not. This is to stop the animation script from constantly
-	 * running on older browsers.
-	 */
-	this.init = function() {
-		// Get the canvas elements
-		this.bgCanvas = document.getElementById('background');
-		this.playerCanvas = document.getElementById('players');
-		this.mainCanvas = document.getElementById('bullets');
-
-		// Test to see if canvas is supported. Only need to
-		// check one canvas
-		if (this.bgCanvas.getContext) {
-			this.bgContext = this.bgCanvas.getContext('2d');
-			this.playerContext = this.playerCanvas.getContext('2d');
-			//this.mainContext = this.mainCanvas.getContext('2d');
-
-			// Initialize objects to contain their context and canvas
-			// information
-			Background.prototype.context = this.bgContext;
-			Background.prototype.canvasWidth = this.bgCanvas.width;
-			Background.prototype.canvasHeight = this.bgCanvas.height;
-
-			Player.prototype.context = this.playerContext;
-			Player.prototype.canvasWidth = this.playerCanvas.width;
-			Player.prototype.canvasHeight = this.playerCanvas.height;
-
-			//Bullet.prototype.context = this.mainContext;
-			//Bullet.prototype.canvasWidth = this.mainCanvas.width;
-			//Bullet.prototype.canvasHeight = this.mainCanvas.height;
-
-			// Initialize the background object
-			this.background = new Background();
-			this.background.init(0,0); // Set draw point to 0,0
-
-			// Initialize the player object
-			this.player = new Player();
-			// Set the player to start near the bottom middle of the canvas
-			var playerStartX = this.playerCanvas.width/2 - imageRepository.player.width;
-			var playerStartY = this.playerCanvas.height/2;// + imageRepository.player.height*2;
-			this.player.init(playerStartX, playerStartY, imageRepository.player.width,
-			               imageRepository.player.height);
-
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	// Start the animation loop
-	this.start = function() {
-		this.player.draw();
-		animate();
-	};
+function rotatePlayer()
+{
+	var mouseX = stage.mouseX;
+	var mouseY = stage.mouseY;
+	var rotationAngle = Math.atan2(mouseY - player.image.y, mouseX - player.image.x);
+	player.image.rotation = rotationAngle * (180/Math.PI);
 }
 
-
-
-/**
- * The animation loop. Calls the requestAnimationFrame shim to
- * optimize the game loop and draws all game objects. This
- * function must be a gobal function and cannot be within an
- * object.
- */
-function animate() {
-	requestAnimFrame( animate );
-	game.background.draw();
-	game.player.move();
-	//game.player.bulletPool.animate(); 
+function playerShoot()
+{
+	if(bulletFrameCounter == 0)
+	{
+		var bullet = new Bullet();
+		bulletArray.push(bullet);
+	}
+	bulletFrameCounter++;
+	if(bulletFrameCounter > bulletFireRate)
+	{
+		bulletFrameCounter = 0;
+	}
 }
 
-KEY_CODES = {
-  32: 'space',
-  65: 'left',
-  87: 'up',
-  68: 'right',
-  83: 'down',
+function mouseClick(e)
+{
+	mousePressed = true;
 }
 
-// Creates the array to hold the KEY_CODES and sets all their values
-// to false. Checking true/flase is the quickest way to check status
-// of a key press and which one was pressed when determining
-// when to move and which direction.
-KEY_STATUS = {};
-for (code in KEY_CODES) {
-  KEY_STATUS[KEY_CODES[code]] = false;
-}
-/**
- * Sets up the document to listen to onkeydown events (fired when
- * any key on the keyboard is pressed down). When a key is pressed,
- * it sets the appropriate direction to true to let us know which
- * key it was.
- */
-document.onkeydown = function(e) {
-  // Firefox and opera use charCode instead of keyCode to
-  // return which key was pressed.
-  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-  if (KEY_CODES[keyCode]) {
-	e.preventDefault();
-	KEY_STATUS[KEY_CODES[keyCode]] = true;
-  }
-}
-/**
- * Sets up the document to listen to ownkeyup events (fired when
- * any key on the keyboard is released). When a key is released,
- * it sets teh appropriate direction to false to let us know which
- * key it was.
- */
-document.onkeyup = function(e) {
-  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-  if (KEY_CODES[keyCode]) {
-    e.preventDefault();
-    KEY_STATUS[KEY_CODES[keyCode]] = false;
-  }
+function mouseUnclick(e)
+{
+	mousePressed = false;
 }
 
-/**	
- * requestAnim shim layer by Paul Irish
- * Finds the first API that works to optimize the animation loop, 
- * otherwise defaults to setTimeout().
- */
-window.requestAnimFrame = (function(){
-	return  window.requestAnimationFrame       || 
-			window.webkitRequestAnimationFrame || 
-			window.mozRequestAnimationFrame    || 
-			window.oRequestAnimationFrame      || 
-			window.msRequestAnimationFrame     || 
-			function(/* function */ callback, /* DOMElement */ element){
-				window.setTimeout(callback, 1000 / 60);
-			};
-})();
+//allow for WASD and arrow control scheme
+function handleKeyDown(e) {
+	//cross browser issues exist
+	if(!e){ var e = window.event; }
+	switch(e.keyCode) {
+		case upKey:		upPressed = true; return false;
+		case downKey:	downPressed = true; return false;
+		case leftKey:	leftPressed = true; return false;
+		case rightKey:	rightPressed = true; return false;
+	}
+}
+
+//allow for WASD and arrow control scheme
+function handleKeyUp(e) {
+	//cross browser issues exist
+	if(!e){ var e = window.event; }
+	switch(e.keyCode) {
+		case upKey:		upPressed = false; break;
+		case downKey:	downPressed = false; break;
+		case leftKey:	leftPressed = false; break;
+		case rightKey:	rightPressed = false; break;
+	}
+}
